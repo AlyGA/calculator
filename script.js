fcb1f6c045fbc9463cd4903b4b13d5101f7a0acd
Aly Anderson <aly779372@gmail.com>
2022-03-17 13:51:27 -0700

    Create functions so that calculator can add, sub, mult, and div

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// Function that allows calculator to add an infinite amount of numbers
+const add = function (...theNums) {
+  return theNums.reduce((totalSum, num) => {
+    return totalSum + num;
+  }, 0);
+};
+
+// Function that allows calculator to subtract an infinite amount of numbers
+const subtract = function (...theNums) {
+  return theNums.reduce((total, num) => {
+    return total - num;
+  });
+};
+
+// Function that allows calculator to multiply an infinite amount of numbers
+const multiply = function (...theNums) {
+  return theNums.reduce((total, num) => {
+    return total * num;
+  }, 1);
+};
+
+// Function that allows calculator to divide an infinite amount of numbers
+const divide = function (...theNums) {
+  return theNums.reduce((total, num) => {
+    return total / num;
+  });
+};
