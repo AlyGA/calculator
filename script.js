ba132dee1b3efc4d5d8024a5638ad47cdac589bc
Aly Anderson <aly779372@gmail.com>
2022-03-20 15:54:47 -0700

    Create more event handlers for buttons

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const minus = document.querySelector(".minus");
 const star = document.querySelector(".star");
 const slash = document.querySelector(".slash");
 const display = document.querySelector(".calc-screen");
+const clear = document.querySelector(".clear");
+const enter = document.querySelector(".enter");
+
+// Initial values
+let initialInput;
+let chosenOperater;
 
 // Function that allows calculator to add an infinite amount of numbers
 const add = function (...theNums) {
@@ -59,19 +65,67 @@ const operate = function (operator, num1, num2) {
   }
 };
 
-function buttonsClicked() {
-  one.addEventListener("click", (event) => {
-    display.textContent = 1;
-  });
-  two.addEventListener("click", (event) => {
-    display.textContent = 2;
-  });
-  three.addEventListener("click", (event) => {
-    display.textContent = 3;
-  });
-  four.addEventListener("click", (event) => {
-    display.textContent = 4;
+// When the buttons are cliked, save the variable or operator
+one.addEventListener("click", () => {
+  display.textContent = one.textContent;
+  initialInput = one.textContent;
+});
+two.addEventListener("click", () => {
+  display.textContent = two.textContent;
+  initialInput = two.textContent;
+});
+three.addEventListener("click", () => {
+  display.textContent = three.textContent;
+  initialInput = three.textContent;
+});
+four.addEventListener("click", () => {
+  display.textContent = four.textContent;
+  initialInput = four.textContent;
+});
+five.addEventListener("click", () => {
+  display.textContent = five.textContent;
+  initialInput = five.textContent;
+});
+six.addEventListener("click", () => {
+  display.textContent = six.textContent;
+  initialInput = six.textContent;
+});
+seven.addEventListener("click", () => {
+  display.textContent = seven.textContent;
+  initialInput = seven.textContent;
+});
+eight.addEventListener("click", () => {
+  display.textContent = eight.textContent;
+  initialInput = eight.textContent;
+});
+nine.addEventListener("click", () => {
+  display.textContent = nine.textContent;
+  initialInput = nine.textContent;
+});
+zero.addEventListener("click", () => {
+  display.textContent = zero.textContent;
+  initialInput = zero.textContent;
+});
+plus.addEventListener("click", () => {
+  display.textContent = plus.textContent;
+  chosenOperater = plus.textContent;
+});
+minus.addEventListener("click", () => {
+  display.textContent = minus.textContent;
+  chosenOperater = minus.textContent;
+});
+star.addEventListener("click", () => {
+  display.textContent = star.textContent;
+  chosenOperater = star.textContent;
+});
+slash.addEventListener("click", () => {
+  display.textContent = slash.textContent;
+  chosenOperater = slash.textContent;
+});
+
+// Once the enter button is pressed, perform the operation
+function enterButton() {
+  enter.addEventListener("click", () => {
+    operator;
   });
 }
-
-buttonsClicked();
